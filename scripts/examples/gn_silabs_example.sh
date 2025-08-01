b81a2e8930d1787680c868844d5b825bd7f94247
Suvesh Pratapa <suvesh.pratapa@silabs.com>
2025-08-01 15:00:05 -0400

    Set `sl_matter_version_str` to use `v1.4-` tagging. Don't override `OPENTHREAD_CONFIG_CHILD_SUPERVISION_CHECK_TIMEOUT`.

--- scripts/examples/gn_silabs_example.sh
+++ scripts/examples/gn_silabs_example.sh
@@ -319,7 +319,7 @@ else
         {
             ShortCommitSha=$(git describe --always --dirty --exclude '*')
             branchName=$(git rev-parse --abbrev-ref HEAD)
-            optArgs+="sl_matter_version_str=\"v1.3-$branchName-$ShortCommitSha\" "
+            optArgs+="sl_matter_version_str=\"v1.4-$branchName-$ShortCommitSha\" "
         } &>/dev/null
     fi
 
